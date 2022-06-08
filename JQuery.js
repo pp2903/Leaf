d8ff8bfcfac41feee9c5c7e7e175b2720c805679
pp2903 <91665293+pp2903@users.noreply.github.com>
2022-06-08 19:46:57 +0530

    Add files via upload

---
+++ JQuery.js
@@ -0,0 +1,18 @@
+// document.onload($('about-para').fadeIn(1000));
+
+    $('.navbar-brand').click(function(){ console.log("clicked");})
+
+$('.heading').hide()
+$('#about-para').hide()
+$('#sign-image').hide()
+
+
+$('.heading').fadeIn(1000)
+$('#about-para').fadeIn(3000)
+
+
+$('#sign-image').fadeIn(1500)
+
+
+
+
